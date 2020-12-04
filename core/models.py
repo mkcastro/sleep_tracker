2004ba96b20855064660ac903302bb672c6cbefe
Mike Castro <mkcastro@gmail.com>
2020-12-05 02:37:28 +0800

    init sleep model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Sleep(models.Model):
+
+
+
+    class Meta:
+        verbose_name = _("Sleep")
+        verbose_name_plural = _("Sleeps")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Sleep_detail", kwargs={"pk": self.pk})
+
+
