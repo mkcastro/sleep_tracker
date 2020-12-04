b93859a7c4d119a90392f1739cc1b8eab4a79013
Mike Castro <mkcastro@gmail.com>
2020-12-05 03:36:32 +0800

    add relationships

--- core/models.py
+++ core/models.py
@@ -4,18 +4,42 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Sleep(models.Model):
+    coffee = models.ForeignKey(
+        "core.Coffee",
+        verbose_name=_("coffee"),
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    bed = models.ForeignKey(
+        "core.Bed",
+        verbose_name=_("bed"),
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    usana = models.ForeignKey(
+        "core.Usana",
+        verbose_name=_("usana"),
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
     class Meta:
         verbose_name = _("Sleep")
         verbose_name_plural = _("Sleeps")
 
     def __str__(self):
-        return self.name
+        return f"You slept for {8} hours"
 
     def get_absolute_url(self):
         return reverse("Sleep_detail", kwargs={"pk": self.pk})
 
 
 class Coffee(models.Model):
+    name = models.TextField(_("name"), default="Folgers")
+
     class Meta:
         verbose_name = _("Coffee")
         verbose_name_plural = _("Coffees")
@@ -28,6 +52,8 @@ class Coffee(models.Model):
 
 
 class Bed(models.Model):
+    name = models.TextField(_("bed"), default="Uratex")
+
     class Meta:
         verbose_name = _("Bed")
         verbose_name_plural = _("Beds")
@@ -40,6 +66,8 @@ class Bed(models.Model):
 
 
 class Usana(models.Model):
+    name = models.TextField(_("usana"), default="Cellsentials")
+
     class Meta:
         verbose_name = _("Usana")
         verbose_name_plural = _("Usanas")
